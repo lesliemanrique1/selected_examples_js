43a7433f285a0be50f20bc711c60df795118847c
leslie m <ljm391@nyu.edu>
2016-12-18 02:11:39 -0500

    Setup

--- socketio/socketio-chat/app.js
+++ socketio/socketio-chat/app.js
@@ -1,20 +1,24 @@
 /* jshint esnext:true */
 const express = require('express');
 const app = express();
+
+
+// Server side, socket io setup 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 app.use(express.static('public'));
 
+//define what server will do on connection 
 io.on('connect', (socket) => {
     // socket represents the connected client
     console.log(socket.id, 'connected');
 
-    // listen 
+    // listen -> define what the server will handle (chat message)
     socket.on('chat message', (data) => {
         // broadcast them to all connected clients
         console.log(data);
         // broadcast to everyone
-        io.emit('chat message', data);
+        io.emit('chat message', data); //emit messages
     });
 
 });
